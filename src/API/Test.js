729ed07b7222a7823993ce309de03b6974be30f8
i-shuan <cjy931246@gmail.com>
2023-05-17 21:02:12 +0800

    modify api

--- src/API/Test.js
+++ src/API/Test.js
@@ -4,16 +4,13 @@ import axios from 'axios';
 parentPort.on('message', async (message) => {
   console.log('Received message:', message);
 
-  try {
-    // Convert the URL string back to a URL object
-    const url = new URL(message.url);
-
+  try {  
     // Use axios to make a GET request with params
-    const response = await axios.get(
-        url.toString(), { 
-            params: message.body 
-        }
-    );
+    const response = await axios({
+        method: 'get',
+        url: message.url,
+        params: message.body,
+    });
 
     // Send the result back to the main thread
     parentPort.postMessage(response.data);
