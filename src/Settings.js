890ee69274ac33520b156e3eab5524d301457945
i-shuan <cjy931246@gmail.com>
2023-05-23 22:01:22 +0800

    add config

--- src/Settings.js
+++ src/Settings.js
@@ -1,11 +1,17 @@
-// Settings.js
 import express from 'express';
 import cors from 'cors';
+import config from 'config';
 
 const createApp = () => {
     const app = express();
-    app.use(cors({ origin: 'http://localhost:3000' }));
+    
+    // 讀取設定檔案的值
+    const allowedOrigins = config.get('allowedOrigins');
+
+    // 設定 CORS
+    app.use(cors({ origin: allowedOrigins }));
     app.use(express.json());
+
     return app;
 };
 
