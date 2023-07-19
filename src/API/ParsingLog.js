8eeceb8140aafb2f39e56fc788db968fe237c51a
i-shuan <cjy931246@gmail.com>
2023-07-19 20:09:05 +0800

    content儲存區塊

--- src/API/ParsingLog.js
+++ src/API/ParsingLog.js
@@ -8,50 +8,41 @@ import getStream from 'get-stream';
 
 
 
-// 建立用於解析警告日誌的函數
 const parseAlarmLog = (line, globalConfig) => {
-    // 建立時間戳記的正則表達式
     const timeStampRegExp = /\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}/;
-    // 建立 alarmid 的正則表達式
     const alarmIdRegExp = /alarmid='([A-Z0-9]+)'/;
 
-    // 檢查行是否包含 "TxName="AlarmCIM""
     if (line.match(/TxName="AlarmCIM"/)) {
-        // 如果當前日誌(currentLog)不為空，則將其添加到已解析的日誌列表(parsedLog)中，然後清空當前日誌(currentLog)
-        if (globalConfig.currentLog.length !== 0) {
-            globalConfig.parsedLog.push(globalConfig.currentLog);
-            globalConfig.currentLog = [];
+        // 如果currentLog有內容，則將其推入parsedLog並清空
+        if (globalConfig.currentLog.length > 0) {
+            const time = globalConfig.currentLog.match(timeStampRegExp);
+            const alarmId = globalConfig.currentLog.match(alarmIdRegExp);
+            if (time !== null && alarmId !== null) {
+                globalConfig.parsedLog.push({
+                    time: time[0],
+                    content: globalConfig.currentLog,
+                    alarmId: alarmId[1]  // 從正則表達式匹配中獲取 alarmid
+                });
+            }
+            globalConfig.currentLog = '';
         }
-        // 將 isAlarmTxName 設置為真
         globalConfig.isAlarmTxName = true;
-    // 檢查行是否包含時間戳記
     } else if (line.match(timeStampRegExp)) {
-        // 如果包含，則將 isAlarmTxName 設置為假
         globalConfig.isAlarmTxName = false;
     }
 
-    // 如果 isAlarmTxName 為真
-    if (globalConfig.isAlarmTxName) {
-        // 從行中匹配時間和 alarmid
-        const time = line.match(timeStampRegExp);
-        const alarmId = line.match(alarmIdRegExp);
-        // 如果時間和 alarmid 都存在
-        if (time !== null && alarmId !== null) {
-            // 將時間、行內容和 alarmid 添加到當前日誌中
-            globalConfig.currentLog.push({
-                time: time[0],
-                content: line,
-                alarmId: alarmId[1]  // 從正則表達式匹配中獲取 alarmid
-            });
-        }
+    // 如果isAlarmTxName為true，則將行追加到currentLog
+    if (globalConfig.isAlarmTxName || line.match(timeStampRegExp)) {
+        globalConfig.currentLog += line + '\n';
     }
 };
 
+
 const ParsingLog = async (filename, res) => {
     var globalConfig = {
-        isAlarmTxName : false,
+        isAlarmTxName: false,
         parsedLog: [],
-        currentLog: []
+        currentLog: ''  // 更改此行
     }
 
     try {
@@ -89,7 +80,7 @@ const ParsingLog = async (filename, res) => {
         .pipe(transformStream)
         // 當轉換流完成處理所有數據後
         .on('finish', () => {
-            
+
             // 確保了即使文件的最後一部分日誌並未以時間戳記結束，它仍然可以被正確地添加到解析的日誌列表中。
             if (globalConfig.currentLog.length !== 0) {
                 globalConfig.parsedLog.push(globalConfig.currentLog);
