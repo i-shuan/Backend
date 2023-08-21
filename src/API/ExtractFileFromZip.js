8dcedf85b835126b11c1a5b9cecd50b2cc46acd7
i-shuan <cjy931246@gmail.com>
2023-08-21 23:15:32 +0800

    zip send chunk

--- src/API/ExtractFileFromZip.js
+++ src/API/ExtractFileFromZip.js
@@ -1,67 +1,66 @@
 import AdmZip from 'adm-zip';
 import path from 'path';
 import fs from 'fs';
+import { Transform } from 'stream';
+import { Readable } from 'stream'; // 导入 Readable 类
 
-const unzip = (zipPath, targetFile) => {
+// A transform stream to mask the content
+class MaskTransform extends Transform {
+    _transform(chunk, encoding, callback) {
+        const maskedChunk = chunk.toString('utf-8').replace(/[a-zA-Z]/g, '*');
+        this.push(maskedChunk);
+        callback();
+    }
+}
 
-    // Normalize and check the path
+const unzipStream = (zipPath, targetFile) => {
     const normalizedZipPath = path.normalize(zipPath);
 
-    // Check if the file exists
     if (!fs.existsSync(normalizedZipPath)) {
         throw new Error('ZIP file does not exist.');
     }
 
-    try {
-        const zip = new AdmZip(normalizedZipPath);
-
-        zip.getEntries().forEach(entry => {
-            console.log(entry.entryName);
-        });
-
-        const zipEntry = zip.getEntry(targetFile);
-
-        if (!zipEntry) {
-            throw new Error('File not found in the ZIP archive.');
-        }
-
-        // 讀取檔案內容
-        const fileData = zip.readFile(zipEntry);
-        return fileData;
-
-    } catch (err) {
-        console.log("Error ::", err);
-        throw err;
+    const zip = new AdmZip(normalizedZipPath);
+    const zipEntry = zip.getEntry(targetFile);
+    
+    if (!zipEntry) {
+        throw new Error('File not found in the ZIP archive.');
     }
-}
 
-const maskContent = (content) => {
-    return content.replace(/[a-zA-Z0-9]/g, '*');
-};
+     // Create a readable stream from the data using stream.Readable.from()
+    const readableStream = Readable.from(async function*() {
+        const chunkSize = 1024; // Adjust the chunk size as needed
+        const data = zipEntry.getData();
+        
+        for (let i = 0; i < data.length; i += chunkSize) {
+            yield data.slice(i, i + chunkSize);
+        }
+        
+        // No need to yield null, just finish the generator.
+    }());
+
+    return readableStream;
+}
 
 const ExtractFileFromZip = async (req, res) => {
     try {
         console.log("req", req.body);
 
-        const fileData = unzip(req.body.zipPath, req.body.targetFile);
-        const textContent = fileData.toString('utf-8');
-        console.log("textContent", textContent)
+        // Get a stream for the file
+        const fileStream = unzipStream(req.body.zipPath, req.body.targetFile);
         
-        // Mask the textContent
-         textContent = maskContent(textContent);
-        // 設置正確的content-type
         res.setHeader('Content-Type', 'text/plain');
         res.setHeader('Content-Disposition', 'attachment; filename=' + path.basename(req.body.targetFile));
-        res.send(textContent); // Send the modified content
+
+        // Pipe the file stream through the mask transform and then to the response
+        fileStream.pipe(new MaskTransform()).pipe(res);
 
     } catch (err) {
         console.error("Error::", err);
-
         if (!res.headersSent) {
             res.status(500).send({error: err.message});
         }
     }
 }
-
 
 export default ExtractFileFromZip;
